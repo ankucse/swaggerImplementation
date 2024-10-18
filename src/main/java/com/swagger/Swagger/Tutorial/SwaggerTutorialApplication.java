e06976bbb7c7f5e9081001c12a4a70ee7860870c
Ankit Sinha <ankitsinha.vkit@gmail.com>
2024-10-18 20:06:25 +0530

    Initial Commit

---
+++ src/main/java/com/swagger/Swagger/Tutorial/SwaggerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.swagger.Swagger.Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerTutorialApplication.class, args);
+    }
+
+}
